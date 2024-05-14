55a99be3487bd0b0c81201802e87e24db2691f0d
Cristian Armijos <106160895+Cristian0072@users.noreply.github.com>
2024-05-13 22:11:21 -0500

    Update test.py

--- SistemaFacturacion/test.py
+++ SistemaFacturacion/test.py
@@ -1,7 +1,1 @@
-from datetime import datetime, timedelta
 
-print(datetime.now().date()+timedelta(days=5))
-print(datetime.now().date())
-
-print(datetime.now().date())
-print(datetime.now().date()+timedelta(minutes=1) == datetime.now().date())
